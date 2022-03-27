49021f5f23bee1963b4b47e69f1adff16ecac0b8
thuaung30 <thuhtetaung.d2@gmail.com>
2022-03-27 14:52:11 +0630

    Update ec2 security group

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "dfu-dev-server" {
   ami           = "ami-0abb8f6b71e7614d5"
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.allow_ssh.name, aws_security_group.allow_http.name]
+  security_groups = [aws_security_group.allow_ssh.name, aws_security_group.allow_http.name, aws_security_group.allow_https.name]
   key_name = aws_key_pair.dfu-dev-server-key.key_name
   
   root_block_device {
