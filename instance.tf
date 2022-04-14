45c23bd28acf7e1f9cee17b2963658231d527259
thuaung30 <thuhtetaung.d2@gmail.com>
2022-04-14 18:25:58 +0630

    Change instance to t2.small

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "dfu-dev-server" {
-  ami           = "ami-0abb8f6b71e7614d5"
-  instance_type = "t2.micro"
+  ami           = "ami-0971b4b88a87adeef"
+  instance_type = "t2.small"
   security_groups = [aws_security_group.allow_ssh.name, aws_security_group.allow_http.name, aws_security_group.allow_https.name]
   key_name = aws_key_pair.dfu-dev-server-key.key_name
   
